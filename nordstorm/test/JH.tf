01cd6f310a3066c5b828fe167d9491acc1451802
Oleh Oleksiiev <aleghyk@gmail.com>
2019-11-15 16:00:09 +0000

    jh ubuntu rc1

--- nordstorm/test/JH.tf
+++ nordstorm/test/JH.tf
@@ -1,6 +1,10 @@
 resource "aws_instance" "JH1" {
     ami = "ami-0cc0a36f626a4fdf5"
     instance_type = "t2.micro"
-	subnet_id = "subnet-eeebf1a3"
-	vpc_security_group_ids="sg-042762992a5670259"
+    subnet_id = "subnet-eeebf1a3"
+    vpc_security_group_ids = ["sg-042762992a5670259"]
+    key_name = "JH"
+    tags = {
+      Name = "JH1"
+    }
 }
